2d4b631aff7d857e32227608f1c2d2663a590775
DamienBen-Faro <damien.benloukil@faro-consulting.com>
2014-04-02 15:13:05 +0200

    resa begin

--- iZicab/ReservationViewController.h
+++ iZicab/ReservationViewController.h
@@ -11,6 +11,24 @@
 @interface ReservationViewController : UIViewController<UIScrollViewDelegate>
 
 
-@property (strong) IBOutlet UIScrollView *scrollView;
+@property (strong)IBOutlet UIScrollView *scrollView;
+@property (strong)IBOutlet UITextField *name;
+@property (strong)IBOutlet UITextField *phone;
+@property (strong)IBOutlet UITextField *startAddress;
+@property (strong)IBOutlet UITextField *endAddress;
+@property (strong)IBOutlet UISwitch    *tiers;
+@property (strong)IBOutlet UIButton    *passBtn;
+@property (strong)IBOutlet UIButton    *luggBtn;
+@property (strong)IBOutlet UIButton    *babySeat;
+@property (strong)IBOutlet UIButton    *paper;
+@property (strong)IBOutlet UIButton    *wifi;
+
+- (IBAction)goToMap:(id)sender;
+- (IBAction)selectDate:(id)sender;
+- (IBAction)selectHour:(id)sender;
+- (IBAction)more:(id)sender;
+- (IBAction)less:(id)sender;
+- (IBAction)setOption:(id)sender;
+- (IBAction)send:(id)sender;
 
 @end
