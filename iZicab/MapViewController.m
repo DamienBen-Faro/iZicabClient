a5abc1feac6e1e4e0ff5b86a25355fd8c933a40e
DamienBen-Faro <damien.benloukil@faro-consulting.com>
2014-04-08 18:07:14 +0200

    beta done

--- iZicab/MapViewController.m
+++ iZicab/MapViewController.m
@@ -270,12 +270,12 @@
     [self.navigationController pushViewController:ctrl animated:YES];
 }
 
-- (void)goBack
+- (IBAction)goBack:(id)sender
 {
     [self.navigationController popViewControllerAnimated:YES];
 }
 
-- (void) goToDash
+- (IBAction)goToDash:(id)sender
 {
     UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main_iPhone" bundle:nil];
     DashboardViewController* ctrl = (DashboardViewController *)[storyboard instantiateViewControllerWithIdentifier:@"DashboardViewController"];
@@ -287,40 +287,7 @@
 {
     
     
-    
-    UIButton *backBtn = [UIButton buttonWithType:UIButtonTypeCustom];
-    UIImage *backBtnImage = [UIImage imageNamed:@"backButton@2x.png"];
-    UIImage *backBtnImagePressed = [UIImage imageNamed:@"backButton@2x.png"];
-    [backBtn setBackgroundImage:backBtnImage forState:UIControlStateNormal];
-    [backBtn setBackgroundImage:backBtnImagePressed forState:UIControlStateHighlighted];
-    [backBtn addTarget:self action:@selector(goBack) forControlEvents:UIControlEventTouchUpInside];
-    backBtn.frame = CGRectMake(0, 0, 50, 70);
-    UIView *backButtonView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 50, 70)];
-    [backButtonView setFrame:CGRectMake(0, 30, 50, 70)];//25, 75
-    [backButtonView addSubview:backBtn];
-    self.navigationItem.leftBarButtonItem = nil;
-    self.navigationItem.hidesBackButton = YES;
-    
-    UIButton *homeBtn = [UIButton buttonWithType:UIButtonTypeCustom];
-    UIImage *homeBtnImage = [UIImage imageNamed:@"menuButton@2X.png"];
-    UIImage *homeBtnImagePressed = [UIImage imageNamed:@"menuButton@2X.png"];
-    [homeBtn setBackgroundImage:homeBtnImage forState:UIControlStateNormal];
-    [homeBtn setBackgroundImage:homeBtnImagePressed forState:UIControlStateHighlighted];
-    [homeBtn addTarget:self action:@selector(goToDash) forControlEvents:UIControlEventTouchUpInside];
-    homeBtn.frame = CGRectMake(0, 0, 50, 70);
-    UIView *homeButtonView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 50, 70)];
-    [homeButtonView setFrame:CGRectMake(270, 30, 50, 70)];//25, 75
-    [homeButtonView addSubview:homeBtn];
-    
-    
-    
-    
-    CustomNavBar *navigationBar = [[CustomNavBar alloc] initWithFrame:CGRectZero];
-    [navigationBar addSubview:backButtonView];
-    [navigationBar addSubview:homeButtonView];
-	[self.navigationController setValue:navigationBar forKey:@"navigationBar"];
-    [(CustomNavBar *)self.navigationController.navigationBar setTitleNavBar:@""];
-    
+    [[self navigationController] setNavigationBarHidden:YES animated:YES];
     
 
 
